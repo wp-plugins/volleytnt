625a0097302843cef2e85e2db800291786d5c85f
belinde <belinde@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-24 09:26:08 +0000

    Aggiunto banner e spiegazioni nel readme.txt. Aggiunto widget dashboard per il conteggio iscritti
    git-svn-id: https://plugins.svn.wordpress.org/volleytnt/trunk@576538 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/volleytnt_home.php
+++ admin/volleytnt_home.php
@@ -15,9 +15,14 @@ class VolleyTNT_Home extends VolleyTNT_AdminPage {
 	
 	public function firstpage() {
 		require_once(ABSPATH . 'wp-admin/includes/dashboard.php');
+		$screen = get_current_screen();
+		$priority = 'core';
 		
-		wp_add_dashboard_widget('volleytnt_iscrizione_rapida', __('Iscrizione rapida', 'volleytnt'), array( $this, 'iscrizione_rapida' ) );	
+//		wp_add_dashboard_widget('volleytnt_iscrizione_rapida', __('Iscrizione rapida', 'volleytnt'), array( $this, 'iscrizione_rapida' ) );	
+//		wp_add_dashboard_widget('volleytnt_iscritti', __('Iscritti', 'volleytnt'), array( $this, 'iscritti' ) );	
 		
+		add_meta_box( 'volleytnt_iscrizione_rapida', __('Iscrizione rapida', 'volleytnt'), array( $this, 'iscrizione_rapida' ), $screen, 'normal', $priority );	
+		add_meta_box( 'volleytnt_iscritti', __('Iscritti', 'volleytnt'), array( $this, 'iscritti' ), $screen, 'side', $priority );
 		
 		wp_dashboard();
 	}
@@ -29,7 +34,57 @@ class VolleyTNT_Home extends VolleyTNT_AdminPage {
 		echo '</p>';
 		$VolleyTNT->call_page_method( 'volleytnt_squadre', 'edit', 0, true );
 	} 
-
+	
+	function iscritti() {
+		global $wpdb;
+		$dati = $wpdb->get_results("SELECT
+									  `s`.`categoria`,
+									  COUNT( DISTINCT `sa`.`squadre_id` ) AS `num`,
+									  COUNT( `a`.`id` ) AS `atl`,
+									  SUM( `sa`.`pagato` ) AS `pag`,
+									  SUM( `sa`.`manleva` ) AS `man`
+									FROM `{$this->prefix}squadre_atleti` AS `sa`
+									  JOIN `{$this->prefix}atleti` AS `a`
+									    ON `a`.`id` = `sa`.`atleti_id`
+									  JOIN `{$this->prefix}squadre` AS `s`
+									    ON `s`.`id` = `sa`.`squadre_id`
+									      AND `s`.`tornei_id` = {$this->opts->corrente}
+									GROUP BY `s`.`categoria`
+									ORDER BY `s`.`categoria`");
+		echo '<table class="widefat"><thead><tr><th>&nbsp;</th>';
+		echo '<th>' . __('Squadre', 'volleytnt') . '</th>';
+		echo '<th>' . __('Atleti', 'volleytnt') . '</th>';
+		echo '<th>' . __('Pagato', 'volleytnt') . '</th>';
+		echo '<th>' . __('Manleva', 'volleytnt') . '</th>';
+		echo '</tr></thead><tbody>';
+		if ( $dati ) {
+			$num = $atl = $pag = $man = 0;
+			foreach ( $dati as $row ) {
+				echo '<tr>';
+				echo '<th>' . $this->l_categorie[ $row->categoria ] . '</th>';
+				echo '<td>' . number_format_i18n( $row->num ) . '</td>';
+				echo '<td>' . number_format_i18n( $row->atl ) . '</td>';
+				echo '<td>' . number_format_i18n( $row->pag ) . '</td>';
+				echo '<td>' . number_format_i18n( $row->man ) . '</td>';
+				echo '</tr>';
+				$num += intval( $row->num );
+				$atl += intval( $row->atl );
+				$pag += intval( $row->pag );
+				$man += intval( $row->man );
+			}
+			echo '</tbody>';
+			echo '<tfoot><tr><th>' . __("Totali:", 'volleytnt') . '</th>';
+			echo '<th>' . number_format_i18n( $num ) . '</th>';
+			echo '<th>' . number_format_i18n( $atl ) . '</th>';
+			echo '<th>' . number_format_i18n( $pag ) . '</th>';
+			echo '<th>' . number_format_i18n( $man ) . '</th>';
+			echo '</tr></tfoot>';
+		} else {
+			echo '<tr><td colspan="5">' . __("Ancora nessuna iscrizione per il torneo corrente.", 'volleytnt') . '</td></tr>';
+			echo '</tbody>';
+		}
+		echo '</table>';
+	}
 	
 }
 ?>
--- readme.txt
+++ readme.txt
@@ -3,19 +3,46 @@ Contributors: belinde
 Donate link: http://volleytnt.belinde.net
 Tags: sport, volley, beachvolley, volleyball, tournament, management
 Requires at least: 3.3
-Tested up to: 3.3
+Tested up to: 3.4.1
 Stable tag: trunk
 
 Manage volley and beach volley tournaments: registration of athletes, management of playing fields, play-offs, the final knockout.
 
 == Description ==
 
-This plugin is under heavy development and not ready for final users!
+This plugin is finally availlable for normal user, yet is always under heavy development. By now is only in italian, but soon will be fully translated in english.
 
-This plugin manages every aspect of volleyball tournaments: registration of athletes, management of playing fields, play-offs, the final knockout. It can handle unlimited editions of the tournament, and you can show the history of all editions played. Dedicated interface is provided for mobile devices, and a native iPhone / Android that can access data is in development.
+This plugin manages every aspect of volleyball tournaments: registration of athletes, management of playing fields, play-offs, the final knockout. It can handle unlimited editions of the tournament, and you can show the history of all editions played.
 
 While still in development this plugin will be free, but will become a fee (approximately € 10) reached a stable version. Free licenses are available for those who help in the development, for example, translating the interface or developing add-ons. 
+
+To properly use this plugin you must follow these steps:
+
+1. Create a new tournement in "Options and tournements" page
+1. Mark it as *current* in the same page
+1. Set playing times in his options
+1. Register players and teams in "Teams" page
+1. When the subscriptions are closed, put the teams in their playing groups
+1. Go in "Matches" page, regenerate the match list and put the matches in their places
+1. When a match has been played, go on "Results" page and write the points
+1. When you play the final knock-out you must go on "Finals" page and write the points
+
+You can use these shortcodes in your posts and pages:
+
+* **[volleytnt_squadre]** the teams list
+* **[volleytnt_calendario]** the calendar
+* **[volleytnt_risultati]** the results of the first phases
+* **[volleytnt_classifiche_gironi]** the standings of the round
+* **[volleytnt_finali]** the final knock-out tree
 
 == Installation ==
 
 Simply use Wordpress standard tools to install this plugin.
+
+== Coming soon ==
+
+English translation.
+
+Dedicated interface for mobile devices.
+
+A native iPhone / Android that can access data.
